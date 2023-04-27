850d0231a44fc56abbe64bc8247569937408bf4a
KimJinkYU <jinkyu1996@hanmail.net>
2023-04-27 21:10:17 +0900

    ToString 변경

--- core/src/main/java/hello/core/order/Order.java
+++ core/src/main/java/hello/core/order/Order.java
@@ -1,5 +1,8 @@
 package hello.core.order;
 
+import lombok.ToString;
+
+@ToString
 public class Order {
 
     private Long memberId;
@@ -50,13 +53,4 @@ public class Order {
         this.discountPrice = discountPrice;
     }
 
-    @Override
-    public String toString() {
-        return "Order{" +
-            "memberId=" + memberId +
-            ", itemName='" + itemName + '\'' +
-            ", itemPrice=" + itemPrice +
-            ", discountPrice=" + discountPrice +
-            '}';
-    }
 }
