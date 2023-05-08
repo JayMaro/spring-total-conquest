030b1605764c6d9499a3baad081b96afc364fda1
KimJinkYU <jinkyu1996@hanmail.net>
2023-05-09 00:31:06 +0900

    롬복과 최신 트렌드

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,17 +1,19 @@
 package hello.core.member;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
-    // @Autowired
-    // 생성자가 하나일 땐 생략 가능
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+    // @Autowired 생성자가 하나일 땐 생략 가능
+    // Lombok을 통해 생략
+//    public MemberServiceImpl(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
 
     @Override
     public void join(Member member) {
